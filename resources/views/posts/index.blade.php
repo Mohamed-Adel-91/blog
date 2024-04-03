ea572f9763b5517827ef63310cd1842d706284fa
MohamedAdel <mohamed101291@gmail.com>
2024-04-03 22:11:34 +0200

    Update views to use named routes and Blade templates
    * resources/views/posts/index.blade.php:
    	+ Replace hard-coded links with named routes
    	+ Use Blade templates for layout and content
    * resources/views/posts/show.blade.php:
    	+ Use Blade templates for layout and content
    * routes/web.php:
    	+ Add named routes for posts index and show
    
    Additionally, remove unused Bootstrap scripts and styles from views.

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,38 +1,9 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Blog App</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Blog App Posts</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Get All Posts</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
-        </div>
-    </nav>
-
-    <!-- Page content -->
+@extends('layouts.app')
+@section('title')
+    All Posts
+@endsection
+@section('content')
+    <!-- All Posts Page content -->
     <div class="container mt-1">
         <div class="text-center py-4">
             <button type="button" class="btn btn-success">Create Post</button>
@@ -56,7 +27,7 @@
                         <td>{{ $post['author'] }}</td>
                         <td>{{ $post['created_at'] }}</td>
                         <td>
-                            <a href="/posts/{{ $post['id'] }}" class="btn btn-info">View</a>
+                            <a href="{{ route('posts.show', $post['id']) }}" class="btn btn-info">View</a>
                             <a href="#" class="btn btn-primary">Edit</a>
                             <a href="#" class="btn btn-danger">Delete</a>
                         </td>
@@ -65,10 +36,4 @@
             </tbody>
         </table>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
-Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
