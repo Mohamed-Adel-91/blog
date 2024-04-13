dbff871f9dc70f7751aab26f446a067bdbfad059
MohamedAdel <mohamed101291@gmail.com>
2024-04-13 03:55:35 +0200

    Updated HTML file for admin dashboard by removing the header section and including the header and footer using Blade templates. Updated routes/auth.php to use named routes. No functional changes.

---
+++ resources/views/admin/scripts.blade.php
@@ -0,0 +1,8 @@
+<script src="adminTemplate/vendor/jquery/jquery.min.js"></script>
+<script src="adminTemplate/vendor/popper.js/umd/popper.min.js"></script>
+<script src="adminTemplate/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="adminTemplate/vendor/jquery.cookie/jquery.cookie.js"></script>
+<script src="adminTemplate/vendor/chart.js/Chart.min.js"></script>
+<script src="adminTemplate/vendor/jquery-validation/jquery.validate.min.js"></script>
+<script src="adminTemplate/js/charts-home.js"></script>
+<script src="adminTemplate/js/front.js"></script>
